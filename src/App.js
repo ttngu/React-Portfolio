fc891abc58cd4a5e229fe60846a509adbfaeb6cd
ttngu <tammyn137@gmail.com>
2021-01-11 20:42:10 -0800

    moved files to main repo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Hero from './components/Hero';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+    
+  );
+}
+
+export default App;
