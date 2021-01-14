5bfd9540a7d8877e2e539006b0fbf5714f9d0bb9
ttngu <tammyn137@gmail.com>
2021-01-13 20:35:49 -0800

    cleaned up more files

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 
 function Home() {
   return (
     <div>
-      
+      <Jumbotron fluid>
+        <Container>
+          <h1>Welcome</h1>
+          <p>
+            This is a portfolio
+          </p>
+        </Container>
+      </Jumbotron>
     </div>
   )
 }
