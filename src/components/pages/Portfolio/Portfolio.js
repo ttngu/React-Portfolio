b4bb32cb1b8e699fb494de4c85fe11909ba16786
ttngu <tammyn137@gmail.com>
2021-01-17 13:38:04 -0800

    added rows and columns

--- src/components/pages/Portfolio/Portfolio.js
+++ src/components/pages/Portfolio/Portfolio.js
@@ -1,32 +1,40 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import './Portfolio.css'
 
 function Portfolio() {
     return (
         <div className="Container">
-            <div className="PortTitle">
-                PORTFOLIO
-            </div>
-            <div className="HrLine">
-            </div>
-            {/* Top Left Port Section */}
-            <div className="BgBlockTop">
-            </div>
-            <div className="PortImgTopL">
-            </div>
-            <div className="SmBlockTop">
-                <div className="PortTextTopL">
+            <Row>
+                <div className="PortTitle">
+                    PORTFOLIO
+                </div>
+                <div className="HrLine">
+                </div>
+            </Row>
+            <Row xs={2}>
+                <Col>
+                    {/* Top Left Port Section */}
+                    <div className="BgBlockTop">
+                    </div>
+                    <div className="PortImgTopL">
+                    </div>
+                    <div className="SmBlockTop">
+                        <div className="PortTextTopL">
+                        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.""
+                        </div>
+                    </div>
+                </Col>
+
+                {/* Top Right Port Section */}
+                <Col>
+                <div className="PortImgTopR">
+                </div>
+                <div className="PortTextTopR">
                 "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.""
                 </div>
-            </div>
-            {/* Top Right Port Section */}
-            <div className="PortImgTopR">
-            </div>
-            <div className="PortTextTopR">
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.""
-            </div>
-
-            
+                </Col>
+            </Row>
         </div>
     )
 }
