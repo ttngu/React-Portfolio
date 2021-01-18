3ab047df12b2c42e2e840c1678e326a155d1f13f
ttngu <tammyn137@gmail.com>
2021-01-17 20:22:48 -0800

    added Contact skels

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Container, Row } from 'react-bootstrap'
+import './Contact.css'
+
 
 function Contact() {
     return (
-        <div>
+        <Container>
+            <Row xs={12}>
+                <div className="LgColorBlock">
+                </div>
+                <div className="SmColorBlock">
+                </div>
+                <div className="Image">
+                </div>
+                <div className="ContactTxt">
+                    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+                    </p>
+                </div>
+            </Row>
+
+        </Container>
+
             
-        </div>
+
     )
 }
 
