b82d79d1554427a442279157e933d6f52c6a5817
ttngu <tammyn137@gmail.com>
2021-01-17 19:29:03 -0800

    added rest of port skel

--- src/components/pages/Portfolio/Portfolio.js
+++ src/components/pages/Portfolio/Portfolio.js
@@ -12,9 +12,9 @@ function Portfolio() {
                 <div className="HrLine">
                 </div>
             </Row>
-            <Row xs={2}>
+            <Row xs={1} md={2}>
+                {/* Top Left Port Section */}
                 <Col>
-                    {/* Top Left Port Section */}
                     <div className="BgBlockTop">
                     </div>
                     <div className="PortImgTopL">
@@ -28,12 +28,40 @@ function Portfolio() {
 
                 {/* Top Right Port Section */}
                 <Col>
-                <div className="PortImgTopR">
-                </div>
-                <div className="PortTextTopR">
-                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.""
-                </div>
+                    <Container>
+                        <div className="PortImgTopR">
+                        </div>
+                        <div className="PortTextTopR">
+                        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.""
+                        </div>
+                    </Container>
                 </Col>
+            </Row>
+            {/* Second Row of Portfolio items */}
+            <Row xs={2}>
+                {/* Bottom Left Port Section */}
+                <Col>
+                    <Container>
+                        <div className="PortImgBottomR">
+                        </div>
+                        <div className="PortTextBottomR">
+                        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.""
+                        </div>
+                    </Container>
+                </Col>
+                {/* Bottom Left Port Section */}
+                <Col>
+                    <div className="BgBlockBottomL">
+                    </div>
+                    <div className="PortImgBottomL">
+                    </div>
+                    <div className="SmBlockBottomL">
+                        <div className="PortTextBottomL">
+                        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.""
+                        </div>
+                    </div>
+                </Col>
+
             </Row>
         </div>
     )
