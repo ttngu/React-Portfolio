482be19ce1368de1c397435d4a2f74cc1c809713
ttngu <tammyn137@gmail.com>
2021-01-19 22:16:38 -0800

    fixed hr line placement

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -9,7 +9,7 @@ function About() {
             <div className="AboutTitle">
                 MEET TAMMY
             </div>
-            <div className="HrLine">
+            <div className="AboutHR">
             </div>
             <div className="Text">
             "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
