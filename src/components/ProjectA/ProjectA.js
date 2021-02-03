e0c0f3e5f8ea9dedaa86371e4d1b54ef33c19822
ttngu <tammyn137@gmail.com>
2021-02-02 20:37:23 -0800

    added a tags to link and styled text

--- src/components/ProjectA/ProjectA.js
+++ src/components/ProjectA/ProjectA.js
@@ -19,9 +19,10 @@ function ProjectA() {
                     <div className="PortTextTopL">
                     <h5>Quarantine Game</h5>
                     A virtual drinking card game, revolving around the theme of 2020 social distancing.
-                    <li>HTML/CSS, Javascript, Socket.io, MySQL, sequelize and Bootstrap</li>
-                    <li>GitHub:</li>
-                    <li>Deployed:</li>
+                    <hr></hr>
+                    <ul>HTML/CSS, Javascript, Socket.io, MySQL, sequelize and Bootstrap</ul>
+                    <ul>GitHub: <a href={'https://github.com/ttngu/QuarantineGame'} className="RepoLink">Link Here</a></ul>
+                    <ul>Deployed: <a href={'https://gentle-cliffs-28390.herokuapp.com/'} className="DeployedLink">Link Here</a></ul>
                     </div>
                 </div>
             </Container>
