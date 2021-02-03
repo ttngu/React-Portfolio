84bbad00f509406c0e50d4bdf9d873ef8cb821b4
ttngu <tammyn137@gmail.com>
2021-02-02 22:08:28 -0800

    updated tech used

--- src/components/ProjectB/ProjectB.js
+++ src/components/ProjectB/ProjectB.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Col, Container, Row, Card, Image } from 'react-bootstrap';
 import './ProjectB.css'
-import '../../images/ProjectB.png'
+import '../../images/ProjectB.gif'
 
-const ScreenCap = require('../../images/ProjectA.png');
+const ScreenCap = require('../../images/ProjectB.gif');
 
 function ProjectA() {
     return (
@@ -20,7 +20,7 @@ function ProjectA() {
                     <h5>Quarantine Game</h5>
                     A virtual drinking card game, revolving around the theme of 2020 social distancing.
                     <hr></hr>
-                    <ul>HTML/CSS, Javascript, Socket.io, MySQL, sequelize and Bootstrap</ul>
+                    <ul>HTML/CSS, JavaScript, jQuery, AJAX, third party API's and server side API's</ul>
                     <ul>GitHub: <a href={'https://github.com/TChristensenDrumz/Home-Cooked-Comfort'} className="RepoLink">Link Here</a></ul>
                     <ul>Deployed: <a href={'https://tchristensendrumz.github.io/Home-Cooked-Comfort/'} className="DeployedLink">Link Here</a></ul>
                     </div>
