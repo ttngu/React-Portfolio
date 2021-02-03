638cfbf19e50671452e558d707ff754b6b05cb22
ttngu <tammyn137@gmail.com>
2021-02-02 19:47:09 -0800

    updated contact info

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -14,8 +14,13 @@ function Contact() {
                 <div className="Image">
                 </div>
                 <div className="ContactTxt">
-                    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-                    </p>
+                    <h4>Get In Touch</h4><hr></hr>
+                    <ul>Phone: 206.639.3200</ul>
+                    <ul>Email: tammyn.137@gmail.com</ul>
+                    <ul><a href={'https://github.com/ttngu'}>GitHub</a></ul>
+                    <ul><a href={'https://www.linkedin.com/in/tamngu/'}>LinkedIn</a></ul>
+                   
+
                 </div>
             </Row>
 
