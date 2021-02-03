bf895254e05ae45c4eb3c4b9b48a7caf8bf8d387
ttngu <tammyn137@gmail.com>
2021-02-02 21:36:08 -0800

    created project card files

---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+
+function ProjectCard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProjectCard
